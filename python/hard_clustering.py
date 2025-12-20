622c21b9f7a3b12d603f7f400380c3161ff64018
Marina <140115877+mhmarina@users.noreply.github.com>
2025-12-19 22:31:26 -0700

    add python scripts

---
+++ python/hard_clustering.py
@@ -0,0 +1,47 @@
+# hard clustering
+import sys
+import random
+import math
+
+'''
+return: 
+k centers obtained from running Lloyd algorithm – k lines (one for each center), each 
+containing m coordinate values for the center
+'''
+def lloyd(k, m, points):
+    # initialize random centers
+    centers = random.sample(points, k)
+    # centers = points[:k]
+    it = 0
+    while it <= 20:
+        clusters  = [[] for _ in range(k)]
+        # for each point in point, find closest center
+        for point in points:
+            min_dist = sys.maxsize
+            min_center_index = -1
+            for i, center in enumerate(centers):
+                # compute distance between point and center
+                dist = math.sqrt(sum((center[j] - point[j])**2 for j in range(m)))
+                if dist < min_dist:
+                    min_dist = dist
+                    min_center_index = i
+            clusters[min_center_index].append(point)
+
+        # compute new centers (of gravity) for each cluster
+        # sum of dimension / order of dimension
+        new_centers = [[] for _ in range(k)]
+        for i, cluster in enumerate(clusters):
+            # compute new center of gravity
+            if len(cluster) > 0:
+                new_centers[i] = ([round(sum(point[j] for point in cluster)/len(cluster),3) for j in range(m)]) # this is the new center of gravity
+
+        # converge if centers havent changed
+        if new_centers == centers:
+            break
+        centers = new_centers
+        it += 1
+
+    # for center in centers:
+    #     for i in range(m):
+    #         center[i] = round(center[i], 3)
+    return centers
---
+++ python/soft_clustering.py
@@ -0,0 +1,54 @@
+# soft clustering
+import math
+import random
+
+# K-means clustering (soft) via Expectation-Maximization (EM) algorithm
+# soft assignment based on what each data point’s responsibility for each cluster
+# given n data points (Data1 . . . Datan) and k centers (x1 . . . xk), we need to construct a k × n 
+
+# centers to clusters
+def expect(HM, centers, points, stiffness, m):
+    totals = [0 for _ in range(len(points))]
+    # expectation
+    # calculate force for each point and center
+    for i in range(len(centers)):
+        for j in range(len(points)):
+            distance =  math.sqrt(sum((centers[i][k] - points[j][k])**2 for k in range(m)))
+            force = math.pow(math.e, -stiffness*distance)
+            HM[i][j] = force
+    # get sum force for each point
+    for i in range(len(centers)):
+        for j in range(len(points)):
+            totals[j] += HM[i][j]
+    # divide HM values by sum
+    for i in range(len(centers)):
+        for j in range(len(points)):
+            HM[i][j] /= totals[j]
+
+# clusters to centers
+def maximize(k, points, HM, m):
+    # x i,j = Hidden Matrix i dot Data j / Hidden Matrix i dot 1 (aka sum of row i)
+    # x i,j where i is the center and j is the datapoint, and HM[i] is the row vector
+    # pythonic dot product: sum(i*j for (i, j) in zip(list1, list2)) or a = A @ B yay !
+    new_centers = [[0 for _ in range(m)] for _ in range(k)]
+    for i in range(k):
+        for j in range(m):
+            new_centers[i][j] = round(sum(a*b for (a, b) in zip(HM[i], [point[j] for point in points])) / sum(HM[i]),3)
+    return new_centers
+
+def soft_clustering(k, m , stiffness, points):
+    # expect
+    HM = [[1/k for _ in range(len(points))] for _ in range(k)] # H[i][j] = responsibiliy of center i on point j
+                                                             # HM = Forcei,j / Sum of all center's forces
+                                                             # Forcei,j = e^(-stiffness*distance(dataj, centeri))
+    # centers = points[:k]
+    centers = random.sample(points, k)
+    i = 0
+    while i <= 100:
+        expect(HM, centers, points, stiffness, m)
+        new_centers = maximize(k, points, HM, m)
+        if new_centers == centers:
+            break
+        centers = new_centers
+        i += 1
+    return centers
