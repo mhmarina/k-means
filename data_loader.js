53bf5822fe2e42c5aea7b5f823c61debb4ad744e
Marina <140115877+mhmarina@users.noreply.github.com>
2026-07-22 21:35:51 -0700

    center data

--- data_loader.js
+++ data_loader.js
@@ -1,4 +1,8 @@
 const toyDataPath = "data/cluster_data.csv" //https://www.kaggle.com/datasets/saquib7hussain/k-mean-cluster-dataset
+const MIN_Y = 10
+const MAX_Y = 550
+const MIN_X = 20
+const MAX_X = 1200
 
 window.addEventListener('load', () => {
     const clearBtn = document.getElementById("btn-clear")
@@ -30,9 +34,16 @@ function parseCSV(text){
     const objects = text.replace(/[\r\t]+/g, "").split("\n")
                 .map((e) => {
                 let obj = e.split(",")
-                return [parseInt(obj[0]), parseInt(obj[1])]
+                return center([parseInt(obj[0]), parseInt(obj[1])])
         })
     return objects
+}
+
+function center(tuple){
+    const x = (tuple[0] * 20 + (MAX_X / 2)) 
+    const y = (tuple[1] * 20 + (MAX_Y / 2)) // multiply to spread out a bit
+
+    return [x,y]
 }
 
 function loadData(){
